95de4b940bc8c34c73c83c5d0d3f1d9441f1bd01
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-04-11 17:49:20 +0000

    [Add]部分テンプレート

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,6 +3,7 @@ class Topic < ApplicationRecord
     has_one_attached :image
 
     validates :title, presence: true,length: { maximum: 30 }
+    validates :image, presence: true
 
     belongs_to :user
     has_many :topic_comments,dependent: :destroy
